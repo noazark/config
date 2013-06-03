c24c1fdc9194e3d07d24a6144c7a36f2b30b4d6c
Noah H. Smith <noah@batteredoats.com>
2013-06-02 19:35:55 -0700

    returning a cloned instance of Config for object keys, fixes #6, fixes #2

--- test/config.js
+++ test/config.js
@@ -5,20 +5,60 @@ describe('config', function () {
   var config = new Config('test/fixtures/', 'test')
 
   it('looks up a configuration key', function () {
-    assert(config.get('foo') === 'bar')
+    assert.equal(config.get('foo'), 'bar')
   })
 
   it('looks up a deep configuration key', function () {
-    assert(config.get('test.key') === 'abcd')
+    assert.equal(config.get('test.key'), 'abcd')
+  })
+
+  describe('when key is an object', function () {
+    before(function () {
+      this.subject = config.get('test.object', {
+        roger: 'roger',
+        butter: {
+          makes: 'good tea',
+          is: 'slippery'
+        }
+      })
+    })
+
+    it('returns an instance of Config', function () {
+      assert(this.subject instanceof Config)
+    })
+
+    it('extends the result', function () {
+      assert(this.subject.get('roger'), 'roger')
+    })
+
+    it('merges the default', function () {
+      assert.equal(this.subject.get('butter.is'), 'slippery')
+    })
+
+    describe('the returned Config', function () {
+      it('looks up a key only on the new instance', function () {
+        assert.equal(this.subject.get('key'), 'efgh')
+      })
+    })
   })
 
   describe('undefined keys', function () {
     it('returns undefined', function () {
-      assert(config.get('does.not.exist') === undefined)
+      assert.equal(config.get('does.not.exist'), undefined)
     })
 
     it('returns default if provided', function () {
-      assert(config.get('does.not.exist', 'myDefault') === 'myDefault')
+      assert.equal(config.get('does.not.exist', 'myDefault'), 'myDefault')
+    })
+
+    describe('when key is an object', function () {
+      before(function () {
+        this.subject = config.get('object.does.not.exist', {hello: 'world'})
+      })
+
+      it('returns an instance of Config', function () {
+        assert(this.subject instanceof Config)
+      })
     })
   });
 })
